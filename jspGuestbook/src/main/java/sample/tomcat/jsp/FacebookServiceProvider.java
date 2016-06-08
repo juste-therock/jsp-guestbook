f912fc60b1bc3d48db657695dcb9a203ef0dae50
Juste Ahouansou <juste@menelic.com>
2016-06-08 18:03:05 +0200

    Implementing Service Providers

--- jspGuestbook/src/main/java/sample/tomcat/jsp/FacebookServiceProvider.java
+++ jspGuestbook/src/main/java/sample/tomcat/jsp/FacebookServiceProvider.java
@@ -1,20 +1,21 @@
 package sample.tomcat.jsp;
 
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
 import org.springframework.social.oauth2.AbstractOAuth2ServiceProvider;
 import org.springframework.social.oauth2.OAuth2Template;
 
-public final class FacebookServiceProvider extends AbstractOAuth2ServiceProvider<FacebookApi> {
+public final class FacebookServiceProvider extends AbstractOAuth2ServiceProvider<Facebook> {
 
-	 public FacebookServiceProvider(String clientId, String clientSecret, ConnectionRepository connectionRepository) {
-	        super("facebook", connectionRepository, 
-	            new OAuth2Template(appId, appSecret,
-	                "https://graph.facebook.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}", 
-	                "https://graph.facebook.com/oauth/access_token"));
-	    }
+	public FacebookServiceProvider(String clientId, String clientSecret, ConnectionRepository connectionRepository) {
+		super(new OAuth2Template(clientId, clientSecret,
+				"https://graph.facebook.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}",
+				"https://graph.facebook.com/oauth/access_token"));
+	}
 
-	    @Override
-	    protected FacebookApi getApi(String accessToken) {
-	        return new FacebookTemplate(accessToken);
-	    }
+	@Override
+	public Facebook getApi(String accessToken) {
+		return new FacebookTemplate(accessToken);
+	}
 }
